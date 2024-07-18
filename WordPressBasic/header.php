73317869714ff96dad837040a1ede33cbddb1b73
yuki <snkyh.boc@gmail.com>
2024-07-18 11:03:53 +0900

    最初のプッシュです。

---
+++ WordPressBasic/header.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="format-detection" content="telephone=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+
+    <!-- meta情報 -->
+    <title>CODO ASSIST</title>
+    <meta name="description" content="世界を「アッ！」と驚かせる当たり前を。お客様の「アッ！」を楽しみに。
+それが我々が目指す製品コンセプトです。
+だからこそ面白く、そして遊びごごろを大切にします。
+お客様の笑顔を見たい。
+アッと驚く体験をぜひ手に取ってみてください。" />
+    <meta name="keywords" content="建築" />
+
+    <!-- ogp -->
+    <meta property="og:title" content="CODO ASSIST" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://xs589126.xsrv.jp/codo_assist_test/" />
+    <!-- <meta property="og:image" content="https://xs589126.xsrv.jp/codo_assist_test/images/salon__picture-pc.jpg" /> -->
+    <meta property="og:site_name" content="CODO ASSIST" />
+    <meta property="og:description" content="CODO ASSIST/世界を「アッ！」と驚かせる当たり前を。お客様の「アッ！」を楽しみに。
+それが我々が目指す製品コンセプトです。
+だからこそ面白く、そして遊びごごろを大切にします。
+お客様の笑顔を見たい。
+アッと驚く体験をぜひ手に取ってみてください。" />
+
+    <!-- noindex -->
+    <meta name="ots" content="noindex" />
+    <!-- nofollow -->
+    <meta name="robots" content="nofollow" />
+
+    <!-- ファビコン -->
+    <link rel="icon" href="./images/favicon.ico" />
+
+    <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Noto+Sans+JP:wght@100..900&display=swap"
+        rel="stylesheet">
+
+    <!-- css -->
+    <link rel="stylesheet" href="./css/styles.css" />
+
+    <!-- JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    <script defer src="./js/script.js"></script>
+
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
+    <header class="">
+        ヘッダー
+    </header>
