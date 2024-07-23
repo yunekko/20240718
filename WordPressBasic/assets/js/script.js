84d840e699f110b2ef5ea9519fef6c7f3be9ae7a
yuki <snkyh.boc@gmail.com>
2024-07-23 16:52:39 +0900

    2回目のプッシュです。

--- WordPressBasic/assets/js/script.js
+++ WordPressBasic/assets/js/script.js
@@ -1,5 +1,5 @@
 // ページトップボタン
-var topBtn = $('.js-pagetop');
+var topBtn = $(".js-pagetop");
 topBtn.hide();
 
 // ページトップボタンの表示設定
@@ -15,19 +15,85 @@ $(window).scroll(function () {
 
 // ページトップボタンをクリックしたらスクロールして上に戻る
 topBtn.click(function () {
-  $('body,html').animate({
-    scrollTop: 0
-  }, 300, 'swing');
+  $("body,html").animate(
+    {
+      scrollTop: 0,
+    },
+    300,
+    "swing"
+  );
   return false;
 });
 
 // スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動。ヘッダーの高さ考慮。)
-$(document).on('click', 'a[href*="#"]', function () {
+$(document).on("click", 'a[href*="#"]', function () {
   let time = 400;
-  let header = $('header').innerHeight();
+  let header = $("header").innerHeight();
   let target = $(this.hash);
   if (!target.length) return;
   let targetY = target.offset().top - header;
-  $('html,body').animate({ scrollTop: targetY }, time, 'swing');
+  $("html,body").animate({ scrollTop: targetY }, time, "swing");
   return false;
+});
+
+// ヘッダーの高さ分だけコンテンツを下げる
+$(function () {
+  const height = $(".js-header").height();
+  $("main").css("margin-top", height);
+});
+
+// ハンバーガーメニュー
+jQuery(function ($) {
+  $(".p-header__hamburger").click(function () {
+    $(this).toggleClass("is-active");
+    // bodyに「.active」class付け外し
+    $("body").toggleClass("active");
+    $(".js-drawer").fadeToggle();
+  });
+  if (window.matchMedia("(max-width: 768px)").matches) {
+    $(".p-header__logo, .p-header__nav-item a").click(function () {
+      $(".p-header__hamburger").removeClass("is-active");
+      $("body").removeClass("active");
+      $(".js-drawer").fadeOut();
+    });
+  }
+});
+
+// Swiper (FV)
+const Swiper1 = new Swiper(".swiper1", {
+  loop: true,
+
+  autoplay: {
+    delay: 0,
+    pauseOnMouseEnter: true,
+    disableOnInteraction: false,
+  },
+
+  speed: 15000,
+  slidesPerView: 1.1,
+  centeredSlides: true,
+
+  breakpoints: {
+    768: {
+      slidesPerView: 1.5,
+    },
+  },
+});
+
+// Swiper (製品情報)
+// サムネイル
+const sliderThumbnail = new Swiper(".slider-thumbnail", {
+  slidesPerView: 4, // サムネイルの枚数
+});
+// スライダー
+const slider = new Swiper(".slider", {
+  loop: true,
+  // 前後の矢印
+  navigation: {
+    nextEl: ".slider-button-next",
+    prevEl: ".slider-button-prev",
+  },
+  thumbs: {
+    swiper: sliderThumbnail,
+  },
 });
